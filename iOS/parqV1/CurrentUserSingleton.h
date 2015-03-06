3632d6e6dacb098b0f75ffd344017c96e6bcffe4
Duncan Riefler <riefler@usc.edu>
2015-03-06 00:26:22 -0800

    Created a singleton class for the currently logged in user

---
+++ iOS/parqV1/CurrentUserSingleton.h
@@ -0,0 +1,13 @@
+//
+//  CurrentUserSingleton.h
+//  parqV1
+//
+//  Created by Duncan Riefler on 3/3/15.
+//  Copyright (c) 2015 Duncan Riefler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CurrentUserSingleton : NSObject
+
+@end
---
+++ iOS/parqV1/CurrentUserSingleton.m
@@ -0,0 +1,13 @@
+//
+//  CurrentUserSingleton.m
+//  parqV1
+//
+//  Created by Duncan Riefler on 3/3/15.
+//  Copyright (c) 2015 Duncan Riefler. All rights reserved.
+//
+
+#import "CurrentUserSingleton.h"
+
+@implementation CurrentUserSingleton
+
+@end
